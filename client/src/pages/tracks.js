8ca4852d8faa8da5b2447d7b1d39d56237aa7c99
Ross Moug <ross.moug@n-able.com>
2021-12-03 11:47:39 +0000

    Lift off I 🚀🐱

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Layout } from '../components';
+import {Layout} from '../components';
+import {gql, useQuery} from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
+
+const GET_TRACKS = gql`
+    query getTracks {
+        tracksForHome {
+            id
+            title
+            thumbnail
+            length
+            modulesCount
+            author {
+                name
+                photo
+            }
+        }
+    }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+    const {loading, error, data} = useQuery(GET_TRACKS);
+
+    return <Layout grid>
+        <QueryResult loading={loading} error={error} data={data}>
+            {data?.tracksForHome?.map(track =>
+                <TrackCard key={track.id} track={track}/>
+            )}
+        </QueryResult>
+    </Layout>
+        ;
 };
 
 export default Tracks;
